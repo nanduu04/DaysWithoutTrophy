152bcaee9289b2ca898c9d96363d6c8fd434c57d
nanduu04 <33555126+nanduu04@users.noreply.github.com>
2020-05-27 23:53:40 -0400

    done

--- js/script.js
+++ js/script.js
@@ -1,46 +1,28 @@
-function getTimeRemaining(endtime) {
-    var t = Date.parse(new Date()) -  1399914157;
-    // var years = Math.floor(8);
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor(( t/(1000 * 60 * 60)) % 24);
-    var days = Math.floor(t/(1000 * 60 * 60 * 24));
-    return {
-      'total': t,
-      // 'years':years,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
-  
-  
-  function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    // var yearsSpan = clock.querySelector('.years');
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
-  
-    function updateClock() {
-      var t = getTimeRemaining(endtime);
-  
-      daysSpan.innerHTML = ('0' + t.days).slice(-4);
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-      // yearsSpan.innerHTML = ('0' + t.hours).slice(-2);
-  
-      // if (t.total <= 0) {
-      //   clearInterval(timeinterval);
-      // }
-    }
-  
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
-  }
-  
-   var deadline = new Date(2019, 43 , 1 , 15, 00, 00);
-  initializeClock('clockdiv', deadline);
+
+window.onload = function() {
+  // Month Day, Year Hour:Minute:Second, id-of-element-container
+  countUpFromTime("Feb 24, 2008 12:00:00", 'countup1'); 
+};
+function countUpFromTime(countFrom, id) {
+  countFrom = new Date(countFrom).getTime();
+  var now = new Date(),
+      countFrom = new Date(countFrom),
+      timeDifference = (now - countFrom);
+    
+  var secondsInADay = 60 * 60 * 1000 * 24,
+      secondsInAHour = 60 * 60 * 1000;
+    
+  days = Math.floor(timeDifference / (secondsInADay) * 1);
+  hours = Math.floor((timeDifference % (secondsInADay)) / (secondsInAHour) * 1);
+  mins = Math.floor(((timeDifference % (secondsInADay)) % (secondsInAHour)) / (60 * 1000) * 1);
+  secs = Math.floor((((timeDifference % (secondsInADay)) % (secondsInAHour)) % (60 * 1000)) / 1000 * 1);
+
+  var idEl = document.getElementById(id);
+  idEl.getElementsByClassName('days')[0].innerHTML = days;
+  idEl.getElementsByClassName('hours')[0].innerHTML = hours;
+  idEl.getElementsByClassName('minutes')[0].innerHTML = mins;
+  idEl.getElementsByClassName('seconds')[0].innerHTML = secs;
+
+  clearTimeout(countUpFromTime.interval);
+  countUpFromTime.interval = setTimeout(function(){ countUpFromTime(countFrom, id); }, 1000);
+}
